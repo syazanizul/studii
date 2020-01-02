d836383b78abf666b9513fab7260ee009e7198c7
syazanizul <syazanizul@gmail.com>
2020-01-03 00:06:36 +0800

    .

--- resources/views/practice.blade.php
+++ resources/views/practice.blade.php
@@ -362,14 +362,18 @@
                                 <div class="control-group" style="width:90%">
                                     <div>
                                         <div style="display:flex;">
-                                            <img class="image_{{($loop->iteration)}}" src="" style="flex-shrink: 0;width:1.6em; padding-bottom: 0.3em; margin:0.1em; visibility: hidden;" alt="tick">
+                                            <div>
+                                                <img class="image_{{($loop->iteration)}}" src="" style="width:1.6em; padding-bottom: 0.3em; margin:0.1em; visibility: hidden;" alt="tick">
+                                            </div>
                                             <label class="control control--radio d-inline-block">{!!$n[0] -> answer!!}
                                                 <input name="input_{{$loop->iteration}}" type="radio" value="1"/>
                                                 <div class="control__indicator"></div>
                                             </label>
                                         </div>
                                         <div style="display:flex;">
-                                            <img class="image_{{($loop->iteration)}}" src="" style="flex-shrink: 0;width:1.6em; padding-bottom: 0.3em; margin:0.1em; visibility: hidden;" alt="tick">
+                                            <div>
+                                                <img class="image_{{($loop->iteration)}}" src="" style="width:1.6em; padding-bottom: 0.3em; margin:0.1em; visibility: hidden;" alt="tick">
+                                            </div>
                                             <label class="d-inline-block control control--radio">{!!$n[1] -> answer!!}
                                                 <input name="input_{{$loop->iteration}}" class="input_answer" type="radio" value="2"/>
                                                 <div class="control__indicator"></div>
@@ -377,7 +381,9 @@
                                         </div>
                                         @if(isset($n[2]))
                                         <div style="display:flex;">
-                                            <img class="image_{{($loop->iteration)}}" src="" style="flex-shrink: 0;width:1.6em; padding-bottom: 0.3em; margin:0.1em; visibility: hidden;" alt="tick">
+                                            <div>
+                                                <img class="image_{{($loop->iteration)}}" src="" style="width:1.6em; padding-bottom: 0.3em; margin:0.1em; visibility: hidden;" alt="tick">
+                                            </div>
                                             <label class="d-inline-block control control--radio">{!!$n[2] -> answer!!}
                                                 <input name="input_{{$loop->iteration}}" class="input_answer" type="radio" value="3"/>
                                                 <div class="control__indicator"></div>
@@ -386,7 +392,9 @@
                                         @endif
                                         @if(isset($n[3]))
                                         <div style="display:flex;">
-                                            <img class="image_{{($loop->iteration)}}" src="" style="flex-shrink: 0;width:1.6em; padding-bottom: 0.3em; margin:0.1em; visibility: hidden;" alt="tick">
+                                            <div>
+                                                <img class="image_{{($loop->iteration)}}" src="" style="width:1.6em; padding-bottom: 0.3em; margin:0.1em; visibility: hidden;" alt="tick">
+                                            </div>
                                             <label class="d-inline-block control control--radio">{!!$n[3] -> answer!!}
                                                 <input name="input_{{$loop->iteration}}" class="input_answer" type="radio" value="4"/>
                                                 <div class="control__indicator"></div>

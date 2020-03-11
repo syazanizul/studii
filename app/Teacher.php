75b98e32720921a684d0288233ace0c23c632d90
syazanizul <syazanizul@gmail.com>
2020-03-12 01:33:42 +0800

    .

--- app/Teacher.php
+++ app/Teacher.php
@@ -11,14 +11,14 @@ class Teacher extends Model
 {
     public static function total_earning(int $type, $teacher_id) {
         if($type==1)    {
-            $question_count = Question::where('creator', $teacher_id)->count();
+            $attempt_count = Attempt::where('creator', $teacher_id)->count();
 //            $accumulated_earning = 0;
 //            dd($question);
 //            foreach($question as $m)   {
 //                $accumulated_earning += $m->earning_per_question();
 //            }
 
-            return $question_count;
+            return $attempt_count;
         }
         return 1;
     }

2e6371b59f943f7e0dd691769aa0e9745c0cd226
syazanizul <syazanizul@gmail.com>
2020-03-28 07:17:09 +0800

    added adsense (1 ad  into practicelink)

--- resources/views/practice.blade.php
+++ resources/views/practice.blade.php
@@ -389,6 +389,18 @@
                                     @endforeach
                                 @endif
                             @endforeach
+
+                                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                                <!-- below_answer -->
+                                <ins class="adsbygoogle"
+                                     style="display:block"
+                                     data-ad-client="ca-pub-7446857168486939"
+                                     data-ad-slot="6628683767"
+                                     data-ad-format="auto"
+                                     data-full-width-responsive="true"></ins>
+                                <script>
+                                    (adsbygoogle = window.adsbygoogle || []).push({});
+                                </script>
                         </div>
 
                     </div>

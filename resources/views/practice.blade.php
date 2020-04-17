53ab2ae1ad9e070e40dabe35c332adccb077c2ab
syazanizul <syazanizul@gmail.com>
2020-04-18 00:11:44 +0800

    .

--- resources/views/practice.blade.php
+++ resources/views/practice.blade.php
@@ -390,19 +390,20 @@
                                 @endif
                             @endforeach
 
-                            <div class="p-2">
-                                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-                                <!-- below_answer -->
-                                <ins class="adsbygoogle"
-                                     style="display:block"
-                                     data-ad-client="ca-pub-7446857168486939"
-                                     data-ad-slot="6628683767"
-                                     data-ad-format="auto"
-                                     data-full-width-responsive="true"></ins>
-                                <script>
-                                    (adsbygoogle = window.adsbygoogle || []).push({});
-                                </script>
-                            </div>
+                        </div>
+
+                        <div class="m-1 p-4">
+                            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                            <!-- below_answer -->
+                            <ins class="adsbygoogle"
+                                 style="display:block"
+                                 data-ad-client="ca-pub-7446857168486939"
+                                 data-ad-slot="6628683767"
+                                 data-ad-format="auto"
+                                 data-full-width-responsive="true"></ins>
+                            <script>
+                                (adsbygoogle = window.adsbygoogle || []).push({});
+                            </script>
                         </div>
                     </div>
                 </div>

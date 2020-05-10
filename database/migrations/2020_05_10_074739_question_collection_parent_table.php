c1829df0710514e099e4ca87d92bcd028da2f0a8
syazanizul <syazanizul@gmail.com>
2020-05-10 19:06:22 +0800

    .

--- database/migrations/2020_05_10_074739_question_collection_parent_table.php
+++ database/migrations/2020_05_10_074739_question_collection_parent_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class QuestionAllocationParentTable extends Migration
+class QuestionCollectionParentTable extends Migration
 {
     /**
      * Run the migrations.

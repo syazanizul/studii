81de07bb0052fa6dc61d670480aad0787b3100e3
syazanizul <syazanizul@gmail.com>
2020-06-07 22:52:45 +0800

    .

--- app/QuestionSetParent.php
+++ app/QuestionSetParent.php
@@ -9,6 +9,6 @@ class QuestionSetParent extends Model
     protected $table = 'question_set';
 
     public function question_set_element()  {
-        return $this->hasMany(QuestionSetElement::class);
+        return $this->hasMany(QuestionSetElement::class, 'question_set_id','id');
     }
 }

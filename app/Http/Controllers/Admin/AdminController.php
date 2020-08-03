d105011ae0b2f812b2d05b535a9c5a6a0f84d6df
syazanizul <syazanizul@gmail.com>
2020-08-03 17:36:27 +0800

    pindah
    to copy and paste all the data from teacher_profile_tracker to profile_tracker

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\ProfileTracker;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
@@ -12,6 +13,35 @@ class AdminController extends Controller
 {
     public function index()
     {
+        $teacher_profile_tracker = DB::table('teacher_profile_tracker')->get();
+
+        foreach($teacher_profile_tracker as $m)   {
+            if($m->edit_profile == 1)    {
+                $p = new ProfileTracker();
+                $p -> role = 2;
+                $p -> user_id = $m->user_teacher_id;
+                $p -> event = 1;
+                $p -> save();
+            }
+
+            if($m->teaching_details == 1)    {
+                $p = new ProfileTracker();
+                $p -> role = 2;
+                $p -> user_id = $m->user_teacher_id;
+                $p -> event = 2;
+                $p -> save();
+            }
+
+            if($m->add_image == 1)    {
+                $p = new ProfileTracker();
+                $p -> role = 2;
+                $p -> user_id = $m->user_teacher_id;
+                $p -> event = 3;
+                $p -> save();
+            }
+
+        }
+
 
         return view('dashboard.admin.admin');
     }
